0f5663b7efa69f9afc3a389b3a589850de3fb7c0
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-10-13 08:55:22 -0500

    Set up  Stack Navigation

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props) {
+class Directory extends Component {
 
-    const renderDirectoryItem = ({item}) => {
+    constructor(props){
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        }
+    }
+
+    //JS keyword "STATIC" sets a method on the class itself rather than the object being instatiated from class
+    //Used by react native to communicate this title to the Navigation Options
+    static navigationOptions = {
+        title: 'Directory'
+    }
+    //-------NAVIGATION PROPS-----
+    //Each screen component in your app is provided with the navigation prop automatically
+    //The prop contains various convenience functions that dispatch navigation actions on the route's router
+
+    render(){
+        const { navigate } = this.props.navigation;
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <ListItem 
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    leftAvatar={{ source: require('./images/react-lake.jpg') }}
+                />
+            )
+        }
         return (
-            <ListItem 
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg') }}
+            <FlatList
+                data={this.state.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
             />
         )
     }
-    return (
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    )
 }
 
 export default Directory;
